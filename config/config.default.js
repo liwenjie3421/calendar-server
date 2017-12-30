85bbe41f8f365e2b9e313629d7cd0dfbe12d653a
liwenjie <liwenjie2@jk.pingan-inc.cn>
2017-12-30 23:22:06 +0800

    不开启CSRF验证

--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,14 @@
-exports.keys = 'aaa';
+module.exports = appInfo => {
+    const config = {};
+    // should change to your own
+    config.keys = appInfo.name + '_1501029269140_9663';
+  
+    // add your config here
+    config.security = {
+        csrf: {
+            enable: false
+        }
+    };
+    return config;
+  };
+  
