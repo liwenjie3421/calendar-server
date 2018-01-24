06352ac4a1b1c2267bb08f2cb8e75460792df480
liwenjie <liwenjie2@jk.pingan-inc.cn>
2018-01-24 18:58:46 +0800

    多余代码

--- app/router.js
+++ app/router.js
@@ -2,6 +2,4 @@ module.exports = app => {
     const { router, controller } = app;
     router.post('/calendarInfo', controller.home.calendarInfo);
     router.get('/calendarInfo', controller.home.calendarInfo);
-
-    router.get('/', controller.home.index);
   };
