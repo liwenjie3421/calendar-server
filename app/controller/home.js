ddb5897fc8f38bc2b1d636a845563ace53a71f13
liwenjie <liwenjie2@jk.pingan-inc.cn>
2018-01-30 09:44:57 +0800

    save db

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,10 +4,11 @@ const path = require('path');
 const moment = require('moment');
 
 const {returnSuccess, returnError} = require('../../lib/plugin/response');
+const {number216} = require('../../lib/plugin/utils');
 class HomeController extends Controller {
     async calendarInfo() {
         const {body: params} = this.ctx.request;
-        const {monthPicker, type, info} = params;
+        const {monthPicker, type, info, tutorInfo} = params;
         const dbpath = path.join(__dirname, '../../db.json');
         if (!fs.existsSync(dbpath)) {
             fs.writeFileSync(dbpath, '', {encoding: 'utf-8'});
@@ -16,7 +17,7 @@ class HomeController extends Controller {
         if (type === 'get') {
             this.getCalendarInfo(monthPicker);
         } else if (type === 'save') {
-            this.saveCalendarInfo(monthPicker, info);
+            this.saveCalendarInfo(monthPicker, info, tutorInfo);
         } else {
             this.noValidateParams(type);
         }
@@ -37,17 +38,34 @@ class HomeController extends Controller {
         }
     }
 
-    saveCalendarInfo(monthPicker, info) {
+    saveCalendarInfo(monthPicker, info, tutorInfo) {
         const filepath = path.join(__dirname, '../../db.json');
         const dbdata = JSON.parse(fs.readFileSync(filepath, {encoding: 'utf-8'}) || '{}');
         const daysNum = moment(monthPicker).daysInMonth();
+        const tutorMap = {};
+        tutorInfo.map(tutor => {
+            if(tutor) {
+                const tutorObj = tutor.split(':');
+                const name = tutorObj[0];
+                const color = tutorObj[1].match(/rgb\((.*)\)/)[1].split(',');
+                
+                tutorMap[`${number216(color[0])}${number216(color[1])}${number216(color[2])}`] = name;
+            }
+        });
 
-        if (typeof info !== 'string' || daysNum !== info.split(',').length) {
-            this.noValidateParams(info);
+        if (daysNum !== info.length) {
+            this.noValidateParams('日期与排班不符');
             return;
         }
 
-        dbdata[monthPicker] = info;
+        dbdata[monthPicker] = {
+            info: info.map(value => {
+                const color = value.color.match(/rgb\((.*)\)/)[1].split(',');
+                value.color = `${number216(color[0])}${number216(color[1])}${number216(color[2])}`;
+                return value;
+            }),
+            tutorMap
+        };
         try {
             fs.writeFileSync(filepath, JSON.stringify(dbdata), {encoding: 'utf-8'});   
             this.returnResult(true);
