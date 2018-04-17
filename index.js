6be4cd0a024978b52cd36f6c5133a62fb6faf8a8
liwenjie <liwenjie2@jk.pingan-inc.cn>
2018-04-17 16:26:14 +0800

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const cors = require('koa2-cors')
 const Router = require('koa-router')
 const KoaBody = require('koa-body')
 
@@ -12,7 +13,14 @@ routes.map(routeObj => {
   const {method, cb, route} = routeObj
   router[method](route, koaBody, cb)
 })
-
+app.use(cors({
+  origin: (ctx) => '*',
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+}))
 app.use(router.routes())
 app.use(router.allowedMethods())
 app.listen(3000)
